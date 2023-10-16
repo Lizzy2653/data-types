71ab2974b1a74ff4262f915f70bc7e521a3ff8db
Lizzy2653 <147071082+Lizzy2653@users.noreply.github.com>
2023-10-16 10:56:27 -0400

    git push

---
+++ Main.java
@@ -0,0 +1,43 @@
+public class Main
+{ public static void main (String)[] args)
+
+    {
+        System.out.println("I did it!");
+        somethingElse();
+    }
+
+    private void somethingElse() {
+    }
+
+    String prompt
+}
+
+
+    howMany = 2;
+    prompt = Hello! ";
+
+    // Operators
+    // Arithmetic: + - * / %
+    //
+
+    prompt = prompt + prompt;
+    howMany = howMany + howMany;
+
+    Integer.bitCount(howMany);
+    prompt.length();
+
+    y = 12;
+}
+    }
+
+int howMany;  //32 bits (4 bytes)
+long howManyStars = 100000000;
+short howManyPeople = 100;
+byte howManyApples = 20
+char itsReallyACharacter;
+
+String longString = "Looooooooong String!";
+String shortString = "Smol";
+
+
+
